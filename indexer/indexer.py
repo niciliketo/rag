62ae9829b8194f7b56a0e6d1163edea41da5bb31
Nicholas Martin <nicholas.martin@marketdojo.com>
2023-09-09 16:59:47 +0100

    working just
    various changes to make this work.

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,29 +1,29 @@
-from retry import retry
-import pandas as pd
-import elasticsearch
-import elasticsearch.helpers
-@retry(elasticsearch.ConnectionError, max_delay=300, delay=5)
-def indexer():
-    es_client = elasticsearch.Elasticsearch(hosts=[{"host": "example_es"}])
-    index_name = "example"
-    number_of_shards = 1
-df_index = pd.read_csv(
-        "/app/data/index_input.csv", na_filter=False
+from langchain.document_loaders import BSHTMLLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch
+
+from config import Paths, openai_api_key
+
+
+def main():
+    loader = BSHTMLLoader(str(Paths.book))
+    data = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
     )
-es_params = {
-        "index": index_name,
-        "body": {
-            "settings": {"index": {"number_of_shards": number_of_shards}}
-        },
-    }
-    if es_client.indices.exists(index=index_name):
-        es_client.indices.delete(index=index_name)
-    es_client.indices.create(**es_params)
-    elasticsearch.helpers.bulk(
-        es_client,
-        df_index.to_dict(orient="records"),
-        doc_type="_doc",
-        index=index_name,
+    documents = text_splitter.split_documents(data)
+
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    db = ElasticVectorSearch.from_documents(
+        documents,
+        embeddings,
+        elasticsearch_url="http://elasticsearch:9200",
+        index_name="elastic-index",
     )
+    print(db.client.info())
+
+
 if __name__ == "__main__":
-    indexer()
+    main()
